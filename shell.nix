01500616be4e308b3fea8156b26a18781ca1ea1a
pingchungchang <pingchungchang@gmail.com>
2025-07-30 14:12:21 +0800

    added shell.nix(but actually not that shell.nix

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ lib, stdenv, cmake, fcitx5, fcitx5-configtool, fcitx5-chinese-addons, gettext, libchewing, fcitx5-table-extra, pkg-config
+  # 確保所有外部依賴都被明確地列在函式簽名中
+  # ... 你可能有的其他依賴，例如 libime, qtbase 等
+}:
+stdenv.mkDerivation {
+	name = "env";
+	src = lib.cleanSource ./.;
+	nativeBuildInputs = [ cmake ];
+	buildInputs = [ fcitx5 fcitx5-configtool fcitx5-chinese-addons gettext libchewing fcitx5-table-extra pkg-config ];
+	cmakeFlags = [
+		"-DFCITX_INSTALL_LIBDIR=${placeholder "out"}/lib/fcitx5"
+		"-DFCITX_INSTALL_PKGDIR=${placeholder "out"}/share/fcitx5"
+	];
+}
