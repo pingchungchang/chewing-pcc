47fa80ba92f781ae84f42db9f35d76471a87cef1
pingchungchang <pingchungchang@gmail.com>
2025-12-16 16:11:41 +0800

    fixed for nixos 25.11

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, cmake, fcitx5, fcitx5-configtool, fcitx5-chinese-addons, gettext, libchewing, fcitx5-table-extra, pkg-config, src
+{ lib, stdenv, cmake, fcitx5, qt6Packages, gettext, libchewing, fcitx5-table-extra, pkg-config, src
   # 確保所有外部依賴都被明確地列在函式簽名中
   # ... 你可能有的其他依賴，例如 libime, qtbase 等
 }:
@@ -6,13 +6,13 @@ stdenv.mkDerivation {
 	name = "env";
 	src = lib.cleanSource ./.;
 	nativeBuildInputs = [ cmake ];
-	buildInputs = [ fcitx5 fcitx5-configtool fcitx5-chinese-addons gettext libchewing fcitx5-table-extra pkg-config ];
+	buildInputs = [ fcitx5 qt6Packages.fcitx5-configtool qt6Packages.fcitx5-chinese-addons gettext libchewing fcitx5-table-extra pkg-config ];
 	cmakeFlags = [
 		"-DFCITX_INSTALL_LIBDIR=${placeholder "out"}/lib/fcitx5"
 		"-DFCITX_INSTALL_PKGDIR=${placeholder "out"}/share/fcitx5"
 	];
 	propagatedBuildInputs = [
-		fcitx5-chinese-addons
+		qt6Packages.fcitx5-chinese-addons
 		fcitx5-table-extra
 		libchewing
 	];
