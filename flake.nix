c893fa035aff4acbc31bfcbfba5e65d8af925db6
pingchungchang <pingchungchang@gmail.com>
2026-05-07 00:55:26 +0800

    added space before backspace in hot fix, refactored language switch

--- flake.nix
+++ flake.nix
@@ -1,40 +1,42 @@
 {
-	description = "A development shell for intel chewing";
+  description = "Fcitx5 Intel Chewing";
 
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-	};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
-	outputs = { self, nixpkgs }:
-
-	let
-		system = "x86_64-linux";
-		pkgs = import nixpkgs {
-			inherit system;
-			config.allowUnfree = true;
-		};
-	in
-	{
-		devShells.${system}.default = pkgs.mkShell {
-			packages = with pkgs; [
-					fcitx5
-					qt6Packages.fcitx5-configtool
-					fcitx5
-					qt6Packages.fcitx5-configtool
-					qt6Packages.fcitx5-chinese-addons
-					gettext
-					libchewing
-					fcitx5-table-extra
-					cmake
-					pkg-config
-					gettext
-
-			];
-			shellHook = ''
-				export FCITX_ADDON_DIRS="$PWD/build/lib:$FCITX_ADDON_DIRS"
-				'';
-
-		};
-	};
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f (
+        import nixpkgs { inherit system; }
+      ));
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        default = pkgs.callPackage ./default.nix { };
+      });
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${pkgs.system}.default ];
+          packages = with pkgs; [
+          fcitx5
+          qt6Packages.fcitx5-configtool
+          fcitx5
+          qt6Packages.fcitx5-configtool
+          qt6Packages.fcitx5-chinese-addons
+          gettext
+          libchewing
+          fcitx5-table-extra
+          cmake
+          pkg-config
+          gettext
+          ];
+          shellHook = ''
+          export FCITX_ADDON_DIRS="$PWD/build/lib:$FCITX_ADDON_DIRS"
+          '';
+        };
+      });
+    };
 }
-
